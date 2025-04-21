9cec314e93ef03d6e52af64e1ab0075a299b7a1a
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-21 11:19:58 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,19 @@
+// Manual
+
+let kata = ["saya", "belajar", "javascript"]
+palindrom = " "
+
+for (let i = kata.length - 1; i >= 0; i--) {
+    palindrom += kata[i];
+  }
+
+console.log(palindrom);
+
+
+// Built In
+
+const array1 = ["one", "two", "three"];
+
+const reversed = array1.reverse();
+console.log("reversed:", reversed);
+  
