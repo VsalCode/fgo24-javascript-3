1775ad8838966e889bc7e4f4faa6661b85564ef4
Muhamad Faisal Ilham <115516624+VsalCode@users.noreply.github.com>
2025-04-21 14:52:46 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -1,13 +1,11 @@
 /*----------------------------------------------------------------
-REVERSE WORDS
+CARA BUILT IN
 ----------------------------------------------------------------*/
 
+// 1. Palindrom
+
 const kata = "malam";
-
 const palindrom = kata.split("").reverse().join("")
-
-console.log(palindrom);
-
 
 if(kata === palindrom){
   console.log("palindrom");
@@ -16,10 +14,48 @@ if(kata === palindrom){
   
 }
 
+// 2. Reverse Words
+
+const reverseBuiltIn = "saya belajar js";
+
+console.log(reverseBuiltIn.split(" ").reverse());
+
 /*----------------------------------------------------------------
-REVERSE WORDS
+CARA MANUAL
 ----------------------------------------------------------------*/
 
-const reverseWords = "saya belajar js";
+// 1. Palindrom
 
-console.log(reverseWords.split(" ").reverse());
+function deteksiPalindrom(inputKata) {
+  kata = inputKata
+  let palindrom = "";
+  
+  for (let i = kata.length - 1; i >= 0; i--) {
+    palindrom += kata[i];
+  }
+  
+  if (kata === palindrom) {
+    console.log("palindrom");
+  } else {
+    console.log("bukan palindrom");
+  }
+}
+
+deteksiPalindrom("siang");
+
+// 2. Reverse Words
+
+function reverseWords(inputKalimat) {
+  const kalimat = inputKalimat.split(' ');
+  const reserve = [];
+  
+  // reverse kalimat
+  for (let i = 0; i < kalimat.length; i++) {
+      const indexTerbalik = kalimat.length - 1 - i;
+      reserve.push(kalimat[indexTerbalik]);
+  }
+  // Gabung kalimat
+  return reserve.join(' ');
+}
+
+console.log(reverseWords("saya belajar js")); 
